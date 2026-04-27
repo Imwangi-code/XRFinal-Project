8aaa8ab227d3a2b287a55049348fc247d708f91e
rtpatel0116 <rtpatel@usc.edu>
2026-04-27 10:06:47 -0700

    Testing Hand tracking
    Testing Hand tracking

--- Assets/Scripts2/HandFollow.cs
+++ Assets/Scripts2/HandFollow.cs
@@ -1,38 +1,24 @@
 using UnityEngine;
-using UnityEngine.XR.Hands;
-using UnityEngine.XR.Management;
+using UnityEngine.XR;
 
 public class HandFollow : MonoBehaviour
 {
-    private XRHandSubsystem handSubsystem;
-    [SerializeField] private bool useRightHand = true;
-    [SerializeField] private XRHandJointID jointId = XRHandJointID.IndexTip;
-
-    private void OnEnable()
+    void Update()
     {
-        handSubsystem = XRGeneralSettings.Instance?.Manager?.activeLoader
-            ?.GetLoadedSubsystem<XRHandSubsystem>();
+        var devices = new System.Collections.Generic.List<InputDevice>();
 
-        if (handSubsystem != null)
-            handSubsystem.updatedHands += OnHandsUpdated;
-    }
+        InputDevices.GetDevicesWithCharacteristics(
+            InputDeviceCharacteristics.HandTracking |
+            InputDeviceCharacteristics.Right,
+            devices
+        );
 
-    private void OnDisable()
-    {
-        if (handSubsystem != null)
-            handSubsystem.updatedHands -= OnHandsUpdated;
-    }
-
-    private void OnHandsUpdated(XRHandSubsystem subsystem,
-        XRHandSubsystem.UpdateSuccessFlags flags,
-        XRHandSubsystem.UpdateType updateType)
-    {
-        var hand = useRightHand ? subsystem.rightHand : subsystem.leftHand;
-        var joint = hand.GetJoint(jointId);
-
-        if (joint.TryGetPose(out Pose pose))
+        foreach (var device in devices)
         {
-            transform.SetPositionAndRotation(pose.position, pose.rotation);
+            if (device.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 pos))
+            {
+                transform.position = pos;
+            }
         }
     }
 }
